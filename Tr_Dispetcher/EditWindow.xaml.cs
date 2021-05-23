1d3ff29ba4382132ab604862d031ab056b1add5a
Андрій Бондарєв <Bondariev.A@ms.sumdu.edu.ua>
2021-05-23 22:53:56 +0300

    Add,edit

--- Tr_Dispetcher/EditWindow.xaml.cs
+++ Tr_Dispetcher/EditWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -29,9 +30,40 @@ namespace Tr_Dispetcher
 
         }
 
+        
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            this.Close();
+            int numb = Convert.ToInt32(number.Text);
+            string stat = station.Text;
+            string dtime = dept_time.Text;
+            string[] arr = dtime.Split(':');
+
+            string ttime = travel_time.Text;
+            string[] mas = ttime.Split(':');
+            int tick = Convert.ToInt32(tickets.Text);
+            if (numb <= 0 || Convert.ToInt32(arr[0]) <= 0 || Convert.ToInt32(arr[0]) > 23 || tick < 0 || Convert.ToInt32(mas[0]) <= 0 || Convert.ToInt32(mas[1]) <= 0 || Convert.ToInt32(arr[1]) <= 0 || Convert.ToInt32(arr[1]) > 23)
+               MessageBox.Show("Номер повинен бути більшим за нуль\nЧас відправлення та прибуття повинні бути більшими за нуль та менші 24\nКількість квитків повинна бути додатньою\nЧас в дорозі повинен бути додатнім \nВведіть коректні дані", "Некоректні вхідні дані", MessageBoxButton.OK, MessageBoxImage.Warning);
+            else
+            {
+                TimeSpan time_of_dept = Convert.ToDateTime(dept_time.Text).TimeOfDay;
+                TimeSpan time_of_travel = Convert.ToDateTime(travel_time.Text).TimeOfDay;
+                ClassTrip trip_to_edit = new ClassTrip(Convert.ToUInt16(numb), stat, time_of_dept, time_of_travel, tick);
+                using (SqlConnection conn = DBUtils.GetDBConnection())
+                {
+                    try
+                    {
+                        conn.Open();
+                       // int k = DBUtils;
+                        //if (k == 1) ;
+                        conn.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                this.Close();
+            }
         }
     }
 }
